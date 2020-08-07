b31ca53e4e618894562c09e1255125a1ed3cea34
Ayush11613587 <38138693+Ayush11613587@users.noreply.github.com>
2020-08-07 22:54:44 +0530

    Ayush TestCase and service added

--- src/main/java/com/cg/healthcare/service/AdminService.java
+++ src/main/java/com/cg/healthcare/service/AdminService.java
@@ -1,11 +1,78 @@
 package com.cg.healthcare.service;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cg.healthcare.dao.DiagnosticCenterRepository;
+import com.cg.healthcare.dao.TestRepository;
+import com.cg.healthcare.entities.DiagnosticCenter;
+import com.cg.healthcare.entities.Test;
 
 @Service
 @Transactional
 public class AdminService {
 
+	/*
+	 * 
+	 * Ayush Gupta's code starts
+	 */
+	@Autowired
+	private TestRepository testRepository;
+	
+	@Autowired
+	private DiagnosticCenterRepository centerRepository;
+	
+	public List<Test> getAllTest(){
+		List<Test> tests=testRepository.findAll();
+		return tests;
+	}
+	
+	public List<DiagnosticCenter> getAllDiagnosticCenter(){
+		List<DiagnosticCenter> centers=centerRepository.findAll();
+		return centers;
+	}
+	public Test addNewTest(Test test) {
+		Test addedTest=testRepository.save(test);
+		return addedTest;
+	}
+	public Test updateTestDetail(Test test) {
+		Test updatedTest=testRepository.save(test);
+		return updatedTest;
+	}
+	
+	public DiagnosticCenter getDiagnosticCenterById(int centerId) {
+		DiagnosticCenter center=centerRepository.findById(centerId).get();
+		return center;
+		
+	}
+	
+	public List<Test> getTestsOfDiagnosticCenter(int centerId){
+		DiagnosticCenter center=getDiagnosticCenterById(centerId);
+		List<Test> testList=new LinkedList<>(center.getTests());
+		return testList;
+	}
+	public List<Test> addTestToDiagnosticCenter(int centerId,List<Test> tests){
+		DiagnosticCenter center=getDiagnosticCenterById(centerId);
+		center.getTests().addAll(tests);
+		centerRepository.save(center);
+		List<Test> updatedTestList=new LinkedList<Test>(center.getTests());
+		return updatedTestList;
+	}
+	public List<Test> removeTestFromDiagnosticCenter(int centerId,List<Test> tests){
+		DiagnosticCenter center=getDiagnosticCenterById(centerId);
+		center.getTests().removeAll(tests);
+		centerRepository.save(center);
+		List<Test> updatedTestList=new LinkedList<Test>(center.getTests());
+		return updatedTestList;
+	}
+	
+	/*
+	 * 
+	 * Ayush Gupta's code ends
+	 */
 }
--- src/test/java/com/cg/healthcare/HealthCareMasterApplicationTests.java
+++ src/test/java/com/cg/healthcare/HealthCareMasterApplicationTests.java
@@ -1,13 +1,93 @@
 package com.cg.healthcare;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.cg.healthcare.dao.DiagnosticCenterRepository;
+import com.cg.healthcare.dao.TestRepository;
+import com.cg.healthcare.entities.DiagnosticCenter;
+import com.cg.healthcare.entities.Test;
+import com.cg.healthcare.service.AdminService;
 
 @SpringBootTest
 class HealthCareMasterApplicationTests {
 
-	@Test
-	void contextLoads() {
+	/*
+	 * Ayush Gupta's code starts
+	 * 
+	 */
+	@Autowired
+	private AdminService adminService;
+	
+	@MockBean
+	private TestRepository testRepository;
+	
+	@MockBean
+	private DiagnosticCenterRepository centerRepository;
+	
+	private static DiagnosticCenter diagnosticCenter;
+	private static Test test1,test2,test3;
+	
+	@BeforeAll
+	public static void init() {
+		diagnosticCenter=new DiagnosticCenter("Akash Diagnostic Center","1223","UP","akash@gmail.com","testing");
+		diagnosticCenter.setId(101);
+		test1=new Test("blood test",1000,"13-17","gm/dl");
+		test2=new Test("Eye Test",1000,"6/6","mm");
+		test3=new Test("LFT",2000,"<1.1","milligm/dl");
 	}
+	@org.junit.jupiter.api.Test
+	public void getAllTestsTest() {
+		List<Test> tests=new LinkedList<>();
+		tests.add(test1);
+		tests.add(test2);
+		when(testRepository.findAll()).thenReturn(tests);
+		assertEquals(2, adminService.getAllTest().size());
+	}
+	
+	@org.junit.jupiter.api.Test
+	public void addNewTestTest() {
+		when(testRepository.save(test1)).thenReturn(test1);
+		assertEquals(test1,adminService.addNewTest(test1));
+	}
+	
+	
+	@org.junit.jupiter.api.Test
+	public void addTestToDiagnosticCenterTest() throws Exception{
+		diagnosticCenter.getTests().add(test1);
+		when(centerRepository.findById(diagnosticCenter.getId())).thenReturn(Optional.of(diagnosticCenter));
+		List<Test> tests=new LinkedList<>();
+		tests.add(test2);
+		tests.add(test3);
+		assertEquals(3,adminService.addTestToDiagnosticCenter(diagnosticCenter.getId(),tests).size());
+	}
+	
+	@org.junit.jupiter.api.Test
+	public void removeTestFromDiagnosticCenterTest() throws Exception {
+		when(centerRepository.findById(diagnosticCenter.getId())).thenReturn(Optional.of(diagnosticCenter));
+		List<Test> tests=new LinkedList<>();
+		tests.add(test2);
+		assertEquals(2,adminService.removeTestFromDiagnosticCenter(diagnosticCenter.getId(), tests).size());
+	}
+	
+	@org.junit.jupiter.api.Test
+	public void getTestsOfDiagnosticCenterTest() throws Exception {
+		when(centerRepository.findById(diagnosticCenter.getId())).thenReturn(Optional.of(diagnosticCenter));
+		assertEquals(2,adminService.getTestsOfDiagnosticCenter(diagnosticCenter.getId()).size());
+	}
+	
+	/*
+	 * Ayush Gupta's code ends
+	 */
 
 }
