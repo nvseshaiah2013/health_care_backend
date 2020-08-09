cfc499a27c713c1e13bfbd13123d421b85b7cb3e
nvseshaiah2013 <26201410+nvseshaiah2013@users.noreply.github.com>
2020-08-09 18:20:09 +0530

    Logs Deleted

--- src/main/java/com/cg/healthcare/dao/TestResultRepository.java
+++ src/main/java/com/cg/healthcare/dao/TestResultRepository.java
@@ -5,12 +5,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
 
 import com.cg.healthcare.entities.TestResult;
 import com.cg.healthcare.entities.TestResultId;
 
 public interface TestResultRepository extends JpaRepository<TestResult, TestResultId>{
-	@Query("SELECT test FROM TestResult test WHERE test.id.appointmentId:= appointmentId")
-	List<TestResult> findAllTests();
+	@Query("SELECT test FROM TestResult test WHERE test.id.appointmentId = ?1")
+	List<TestResult> findAllTests(int appointmentId);
 }
