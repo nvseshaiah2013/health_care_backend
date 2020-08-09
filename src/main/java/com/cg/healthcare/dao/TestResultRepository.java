4660a94d13861dcd81b251d43d7fdc1f4b20ff19
Patnam Madhu <patnammadhu1999@gmail.com>
2020-08-09 17:13:31 +0530

    Test

--- src/main/java/com/cg/healthcare/dao/TestResultRepository.java
+++ src/main/java/com/cg/healthcare/dao/TestResultRepository.java
@@ -1,12 +1,16 @@
 package com.cg.healthcare.dao;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
 import com.cg.healthcare.entities.TestResult;
 import com.cg.healthcare.entities.TestResultId;
 
 public interface TestResultRepository extends JpaRepository<TestResult, TestResultId>{
-	
-
+	@Query("SELECT test FROM TestResult test WHERE test.id.appointmentId:= appointmentId")
+	List<TestResult> findAllTests();
 }
