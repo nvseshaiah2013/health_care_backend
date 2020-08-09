ad6f25a79b14cc47656cdded33bae47da877418e
manishmajji <majjimanish@gmail.com>
2020-08-09 13:20:11 +0530

    Test cases

--- src/main/java/com/cg/healthcare/service/DiagnosticCenterService.java
+++ src/main/java/com/cg/healthcare/service/DiagnosticCenterService.java
@@ -1,11 +1,36 @@
 package com.cg.healthcare.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cg.healthcare.dao.BedRepository;
+import com.cg.healthcare.entities.Bed;
 
 @Service
 @Transactional
-public class DiagnosticCenterService {
 
+
+public class DiagnosticCenterService {
+	
+	BedRepository dao;
+
+	public List<Bed> getBeds() {
+		return dao.getAllBeds();
+		
+	}
+	public List<Bed> admitPatient() {
+		return dao.getVacantBeds();
+		
+	}
+	public List<Bed> canNotAdmitPatient(){
+		return dao.getVacantBeds();
+	}
+	public List<Bed> dischargePatient() {
+		return dao.deallocateAssignedBed();
+		
+	}
 }
